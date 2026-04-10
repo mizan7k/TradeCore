3d7bd78468575ef7e2c3b6190e90ea82ee9bca08
Mizanur Rahman <private.mizan@gmail.com>
2026-04-10 07:43:38 +0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: './', // <--- THIS LINE FIXES THE BLANK PAGE
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
